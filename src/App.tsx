53f8da6741012352e7d80e9dcf3cd9e959e76e46
Zohir <vasv29834@gmail.com>
2024-05-29 23:25:32 +0500

    Some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center relative">
+      <div className="w-3/4 rounded-3xl main-content">
+        <div className=" text-white font-normal text-3xl p-7 box-border">
+          T<span>Chain</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
