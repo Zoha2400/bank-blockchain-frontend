5114abb5ffb833c82f86b089b1cc2a4a2082c238
Zohir <vasv29834@gmail.com>
2024-05-29 23:37:15 +0500

    some fucking design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import { Icon } from "@iconify/react";
 import "./App.css";
 
 function App() {
   return (
     <div className="w-full min-h-screen flex justify-center items-center relative">
-      <div className="w-3/4 rounded-3xl main-content">
-        <div className=" text-white font-normal text-3xl p-7 box-border">
-          T<span>Chain</span>
+      <div className="w-3/4 rounded-5xl main-content">
+        <div className=" text-white font-normal text-4xl p-9 box-border flex justify-between">
+          <p>
+            T<span className=" font-semibold ">Chain</span>
+          </p>
+          <button>
+            <Icon icon="hugeicons:notification-03" />
+          </button>
         </div>
       </div>
     </div>
