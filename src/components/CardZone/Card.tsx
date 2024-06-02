4cc920fde31ab14c049e01d7ceb6749433fe777c
Zohir <vasv29834@gmail.com>
2024-06-02 17:19:48 +0500

    Visa

--- src/components/CardZone/Card.tsx
+++ src/components/CardZone/Card.tsx
@@ -2,9 +2,9 @@ import { Icon } from "@iconify/react";
 
 function Card() {
   return (
-    <div className=" min-w-96 h-52 rounded-2xl z-10 p-5 bg-purple-600 relative text-white flex flex-col overflow-hidden justify-between">
+    <div className=" min-w-96 h-52 rounded-2xl z-10 p-5 bg-gray-400 relative text-white flex flex-col overflow-hidden justify-between">
       <div className="icon  h-16 flex justify-start items-center">
-        <Icon icon="logos:visa" className="w-24 h-24" />
+        <Icon icon="logos:visa" className="w-24 h-24 logo" />
       </div>
       <p className="number text-2xl z-10">4688 **** **** 1684</p>
       {/* <p className="text-xl ">Main Card</p> */}
