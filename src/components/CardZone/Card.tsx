0e2409b2f9f247b14ddb4580922a87957a20bb2d
Zohir <vasv29834@gmail.com>
2024-06-02 15:34:43 +0500

    Card Desgin

--- src/components/CardZone/Card.tsx
+++ src/components/CardZone/Card.tsx
@@ -1,7 +1,14 @@
+import { Icon } from "@iconify/react";
+
 function Card() {
   return (
-    <div>
-      <p className="text-xl w-96 h-52 rounded-2xl z-10 p-3 card">Your Card</p>
+    <div className=" min-w-96 h-52 rounded-2xl z-10 p-5 bg-purple-600 text-white flex flex-col justify-between">
+      <div className="icon  h-16 flex justify-start items-center">
+        <Icon icon="logos:visa" className="w-24 h-24" />
+      </div>
+
+      <p className="number text-2xl">4688 **** **** 1684</p>
+      {/* <p className="text-xl ">Main Card</p> */}
     </div>
   );
 }
